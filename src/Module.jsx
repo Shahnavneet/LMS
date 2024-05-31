80aba0679dc5fa4720f850135f021643c4adadd1
shahnavneet <navneetshah41@gmail.com>
2024-05-31 13:24:46 +0530

    changes in module

--- src/Module.jsx
+++ src/Module.jsx
@@ -1,7 +1,23 @@
 const Module = () => {
   return (
     <>
-      <h1>This is your Module</h1>
+      <div>
+        <div className="from-sky-300	">
+          <h2>MERN STACK</h2>
+          <div className="flex justify-between">
+            <span>remaining fees ($0)</span>
+            <span>Your Progress</span>
+          </div>
+          <div className="w-full h-4 border rounded-md">
+            <div className="w-2/4 h-full bg-green-500 border rounded-md"></div>
+            <div className="flex flex-col gap-4">
+              <span>50% Completed</span>
+              <span>Registered on: 5 Feb,2024</span>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
